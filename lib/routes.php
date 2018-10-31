2d49042d829beef95e53857a85363c482b3b6d78
Victor Hugo <vct.bta@gmail.com>
2018-10-31 00:08:15 -0300

    Generate routes with post type slugs

--- lib/routes.php
+++ lib/routes.php
@@ -1,4 +1,13 @@
 <?php
 use Brain\Cortex;
+use Brain\Cortex\Route\RouteCollectionInterface;
+use victorhugobatista\WpEruptor\BO\PostTypeRoute;
 
 Cortex::boot();
+
+add_action('cortex.routes', function(RouteCollectionInterface $routes) {
+    $allPostTypes = get_post_types([], 'objects');
+    foreach ($allPostTypes as $postType) {
+        new PostTypeRoute($postType, $routes);
+    }
+});
