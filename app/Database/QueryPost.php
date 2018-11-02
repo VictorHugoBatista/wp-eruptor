af469fc501b11037b44bd2ae1ae870344b07ff7a
Victor Hugo <vct.bta@gmail.com>
2018-11-02 15:19:59 -0300

    Document the query post class

--- app/Database/QueryPost.php
+++ app/Database/QueryPost.php
@@ -3,12 +3,29 @@ namespace victorhugobatista\WpEruptor\Database;
 
 use WP_Query;
 
+/**
+ * Ressponsible for get one post passing the post type
+ * name and the post name on constructor.
+ */
 class QueryPost
 {
     private $postTypeName;
     private $postName;
+
+    /**
+     * Quere generated on constructor.
+     *
+     * @var WP_Query
+     */
     private $query;
 
+    /**
+     * Do an WP_Query by one post with the post type
+     * name and the post name pass to a property.
+     *
+     * @param string $postTypeName Post type name, showed on single page slugs.
+     * @param string $postName Post name, showed on single page slugs.
+     */
     public function __construct($postTypeName, $postName)
     {
         $this->postTypeName = $postTypeName;
@@ -20,11 +37,21 @@ class QueryPost
         ]);
     }
 
+    /**
+     * Verify if the return of WP_Query is not empty.
+     *
+     * @return boolean
+     */
     public function postExists()
     {
         return ! empty($this->query->posts);
     }
 
+    /**
+     * Return the queried post or false if not exists.
+     *
+     * @return boolean|WP_Post
+     */
     public function getPost()
     {
         if (empty($this->query->posts)) {
