93b8e87ced50bcfab9d037165ec543b9b192314d
Victor Hugo <vct.bta@gmail.com>
2018-11-02 17:14:18 -0300

    Change Eruptor constructor by Eruptor::initialize() static method to boot lib

--- app/Eruptor.php
+++ app/Eruptor.php
@@ -11,24 +11,35 @@ use victorhugobatista\WpEruptor\BO\PostTypeRoute;
 class Eruptor
 {
     /**
+     * Flag who control if the library is initialized.
+     *
+     * @var boolean
+     */
+    private static $initialized = false;
+
+    /**
      * Initialize the cortex routes. Boot the cortex feature by
      * default, but is possible deactivate this behaviour.
+     * Can be called one time per request.
      * 
      * @todo To receive the root directory to read the templates.
      * @todo To receive what post types have to be added to routes.
      *
      * @param boolean $bootCortex Initialize the cortex feature if true (default).
      */
-    public function __construct($bootCortex = true)
+    public static function initialize($bootCortex = true)
     {
-        if ($bootCortex) {
-            Cortex::boot();
-        }
-        add_action('cortex.routes', function(RouteCollectionInterface $routes) {
-            $allPostTypes = get_post_types([], 'objects');
-            foreach ($allPostTypes as $postType) {
-                new PostTypeRoute($postType, $routes);
+        if (! static::$initialized) {
+            if ($bootCortex) {
+                Cortex::boot();
             }
-        });
+            add_action('cortex.routes', function(RouteCollectionInterface $routes) {
+                $allPostTypes = get_post_types([], 'objects');
+                foreach ($allPostTypes as $postType) {
+                    new PostTypeRoute($postType, $routes);
+                }
+            });
+            static::$initialized = true;
+        }
     }
 }
